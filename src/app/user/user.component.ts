76d91fc23a6b409679f8fb47728f32d047d7aff5
Kevin M. <k.mary@live.be>
2024-06-24 16:42:01 +0200

    All components with module

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,15 +4,14 @@ import { type User } from './user.model';
 import { CardComponent } from "../shared/card/card.component";
 
 @Component({
-    selector: 'app-user',
-    standalone: true,
-    templateUrl: './user.component.html',
-    styleUrl: './user.component.css',
-    imports: [CardComponent]
+  selector: 'app-user',
+  standalone: false,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-  @Input({required: true}) selected!: boolean;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
